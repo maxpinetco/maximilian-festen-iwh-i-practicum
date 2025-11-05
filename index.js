6d74956c0a0147d2606ff6edc3a4e5ec650ff86d
maxpinetco <max@pinetco.com>
2025-11-05 14:51:53 +0530

    Failed to Create Error Solved

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+  });
+});
+
 app.post("/update-cobj", async (req, res) => {
   try {
     const { book_title, book_author, book_description } = req.body;
